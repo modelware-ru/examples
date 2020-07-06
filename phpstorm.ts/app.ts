637c58222d14af308e9431fef1a0158d086c349a
Denis Ivanov <info@modelware.ru>
2020-07-06 12:14:51 +0300

    ts - demo

---
+++ phpstorm.ts/app.ts
@@ -0,0 +1,13 @@
+class MyClass {
+    public render (divId: string, text: string) {
+        var el :HTMLElement | null = document.getElementById(divId);
+        if (el) {
+            el.innerText = text;
+        }
+    }
+}
+
+window.onload = () => {
+    var myClass = new MyClass();
+    myClass.render("content", "Hello World");
+};
