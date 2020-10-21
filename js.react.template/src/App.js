85f15cf49927d2d08cec932049bc73eac56af4a8
Denis Ivanov <info@modelware.ru>
2020-10-21 14:14:31 +0300

    ts.react.template: init

---
+++ js.react.template/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+            </div>
+        );
+    }
+
+}
---
+++ js.react.template/webpack.config.js
@@ -0,0 +1,105 @@
+const webpack = require('webpack');
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+const config = {
+    mode: 'development',
+    entry: [
+        './src/index.js',
+    ],
+    devServer: {
+        hot: true,
+        contentBase: path.resolve(__dirname, 'dist'),
+        historyApiFallback: true,
+        port: 10000,
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+            {
+                test: /\.s[ac]ss$/,
+                exclude: [/node_modules/, /\.module.(s(a|c)ss)$/],
+                loader: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                include: [/\.module.(s(a|c)ss)$/],
+                loader: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[name]__[local]___[hash:base64:5]',
+                            },
+                            importLoaders: 2,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.json']
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['!index.html'],
+            verbose: true,
+        }),
+        new HtmlWebpackPlugin({template: './src/index.html'}),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: 'src/asset/img',
+                    to: 'asset/img',
+                },
+            ]
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devtool: 'inline-source-map',
+    stats: {
+        all: false,
+        builtAt: true,
+        errors: true,
+        errorDetails: true,
+    },
+};
+
+module.exports = config;
